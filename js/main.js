0f6aadff15ae7c3bf0323bbf0daa56d8c5e33911
benjaminwand <benjamin.wand@web.de>
2020-06-30 22:25:54 +0200

    better for keyboard input, jslint

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // hides no-js-warning
   $(".no-js").hide();
 
   // filter food items with checkboxes
-  $(".choices :checkbox").click(function () {
+  $(".choices :checkbox").click(function() {
     $(".polaroid").hide();
     if ($("input[name=category]:checked").length) {
-      $("input:checked").each(function () {
+      $("input:checked").each(function() {
         $("." + $(this).val()).show();
       });
     }
